7febaf31999fafb0e2810b4f25600674fbed351d
JebySun <jebysun@126.com>
2017-11-25 18:27:03 +0800

    第一个简易版本

---
+++ menu_data.js
@@ -0,0 +1,88 @@
+var json = [
+        {
+            "menuId": "3771fc08-a26a-4ff6-a20e-29723071dbdc",
+            "menu": "菜单1",
+            "url": "/test",
+            "fatherId": null,
+            "createTime": 1511426632000,
+            "updateTime": 1511530240000,
+            "children": [
+                {
+                    "menuId": "97c31094-4486-4d02-b86c-4ac303f4d556",
+                    "menu": "回合",
+                    "url": "7",
+                    "fatherId": "3771fc08-a26a-4ff6-a20e-29723071dbdc",
+                    "createTime": 1511412468000,
+                    "updateTime": 1511418174000,
+                    "children": [
+                        {
+                            "menuId": "aabd7623-bc30-4106-a4fb-60bd3dbb275d",
+                            "menu": "哈哈",
+                            "url": "45",
+                            "fatherId": "97c31094-4486-4d02-b86c-4ac303f4d556",
+                            "createTime": 1511412792000,
+                            "updateTime": 1511412792000,
+                            "children": [
+                                {
+                                    "menuId": "4c660999-32b8-49a8-a526-4878830a5842",
+                                    "menu": "3233",
+                                    "url": "333",
+                                    "fatherId": "aabd7623-bc30-4106-a4fb-60bd3dbb275d",
+                                    "createTime": 1511412848000,
+                                    "updateTime": 1511412848000,
+                                    "children": []
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    "menuId": "fa6586fe-0ddd-4767-ad86-9214728d4110",
+                    "menu": "热吻",
+                    "url": "444",
+                    "fatherId": "3771fc08-a26a-4ff6-a20e-29723071dbdc",
+                    "createTime": 1511412679000,
+                    "updateTime": 1511491954000,
+                    "children": [
+                        {
+                            "menuId": "4c660999-32b8-49a8-a526-4878830a5842",
+                            "menu": "菜单2-1",
+                            "url": "333",
+                            "fatherId": "aabd7623-bc30-4106-a4fb-60bd3dbb275d",
+                            "createTime": 1511412848000,
+                            "updateTime": 1511412848000,
+                            "children": []
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            "menuId": "sddd",
+            "menu": "地方",
+            "url": "cc",
+            "fatherId": null,
+            "createTime": 1511424886000,
+            "updateTime": 1511424886000,
+            "children": [
+                {
+                    "menuId": "4c660999-32b8-49a8-a526-4878830a5842",
+                    "menu": "菜单2-1-1",
+                    "url": "333",
+                    "fatherId": "aabd7623-bc30-4106-a4fb-60bd3dbb275d",
+                    "createTime": 1511412848000,
+                    "updateTime": 1511412848000,
+                    "children": []
+                },
+                {
+                    "menuId": "4c660999-32b8-49a8-a526-4878830a5842",
+                    "menu": "菜单2-1-2",
+                    "url": "333",
+                    "fatherId": "aabd7623-bc30-4106-a4fb-60bd3dbb275d",
+                    "createTime": 1511412848000,
+                    "updateTime": 1511412848000,
+                    "children": []
+                }
+            ]
+        }
+    ];
